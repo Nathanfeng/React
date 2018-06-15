c3a1d23630a7dd29e2d25b670e38c34404c08dea
Nathan Feng <nathanlfeng@gmail.com>
2018-06-15 14:29:30 -0400

    built visibility toggle

--- react_projects/indecision-app/public/scripts/app.js
+++ react_projects/indecision-app/public/scripts/app.js
@@ -1,3 +1,46 @@
 "use strict";
 
 console.log("hi");
+
+var app = {
+  title: "Visibility Toggle"
+};
+
+var appRoot = document.getElementById("app");
+
+var state = false;
+
+var changeState = function changeState() {
+  if (state) {
+    state = false;
+  } else {
+    state = true;
+  };
+  run();
+};
+
+var run = function run() {
+  var template = React.createElement(
+    "div",
+    null,
+    React.createElement(
+      "h1",
+      null,
+      app.title
+    ),
+    React.createElement(
+      "button",
+      { onClick: changeState },
+      state ? "show details" : "hide details"
+    ),
+    state && React.createElement(
+      "p",
+      null,
+      "peek a boo!"
+    )
+  );
+
+  ReactDOM.render(template, appRoot);
+};
+
+run();
--- react_projects/indecision-app/src/playground/build-it-visible.js
+++ react_projects/indecision-app/src/playground/build-it-visible.js
@@ -1,1 +1,32 @@
 console.log("hi")
+
+const app = {
+  title: "Visibility Toggle"
+}
+
+const appRoot = document.getElementById("app")
+
+let state = false;
+
+const changeState = () => {
+  if (state) {
+    state = false;
+  } else {
+    state = true;
+  };
+  run();
+}
+
+const run = () => {
+  const template = (
+    <div>
+      <h1>{app.title}</h1>
+      <button onClick={changeState}>{state ? "show details" : "hide details"}</button>
+      {state && <p>peek a boo!</p>}
+    </div>
+  );
+
+  ReactDOM.render(template, appRoot)
+}
+
+run()
