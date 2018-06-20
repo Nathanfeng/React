7952d3c8858c34e91704e238976c7e631c6d118d
Nathan Feng <nathanlfeng@gmail.com>
2018-06-20 19:05:56 -0400

    built out webpack for project

---
+++ react_projects/indecision-app/src/components/IndecisionApp.js
@@ -0,0 +1,85 @@
+import React from 'react'
+
+import AddOption from './AddOption';
+import Header from './Header';
+import Action from './Action';
+import Options from './Options';
+
+class IndecisionApp extends React.Component {
+
+  state = {
+    options: []
+  };
+
+
+  handleDeleteOptions = () => {
+    this.setState(() => ({ options: []}))
+  };
+
+  handleDeleteOption = (option) => {
+    this.setState((prevState) => ({
+      options: prevState.options.filter(el => el !== option)
+    }));
+  };
+
+  handlePick = () => {
+    let options = this.state.options
+    let pick = options[Math.floor(Math.random() * options.length)];
+    alert(`${pick}`)
+  };
+
+  handleAddOption = (option) => {
+    if (!option) {
+      return 'Enter valid input';
+    } else if (this.state.options.indexOf(option) > -1) {
+      return "already entered";
+    }
+
+    this.setState((prevState)=> ({
+      options: prevState.options.concat(option)
+    }));
+  };
+
+  componentDidMount() {
+    try {
+      const json = localStorage.getItem('options');
+      const options = JSON.parse(json);
+      if (options) {
+        this.setState(() => ({options}));
+      }
+    } catch (e) {
+      //do nothing
+    }
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.options.length !== this.state.options.length) {
+      const json = JSON.stringify(this.state.options);
+      localStorage.setItem('options', json);
+    }
+  };
+
+  render() {
+    const title = 'Indecision';
+    const subtitle = 'Put your life in the hands of a computer';
+    return (
+      <div>
+        <Header subtitle={subtitle}/>
+        <Action
+          hasOptions={this.state.options.length > 0}
+          handlePick={this.handlePick}
+        />
+        <Options
+          options={this.state.options}
+          handleDeleteOptions={this.handleDeleteOptions}
+          handleDeleteOption={this.handleDeleteOption}
+        />
+        <AddOption
+          handleAddOption={this.handleAddOption}
+        />
+      </div>
+    );
+  }
+};
+
+export default IndecisionApp;
